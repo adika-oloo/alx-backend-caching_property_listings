5ee2e22215739583335b997cbc3a08c19a96e530
Calvince Adika <caadika@gmail.com>
2025-08-31 20:27:36 +0300

    Wrap property_list JsonResponse in {'data': ...} to match rubric

--- properties/views.py
+++ properties/views.py
@@ -2,12 +2,13 @@ from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 from .models import Property
 
-@cache_page(60 * 15)  # Cache the response for 15 minutes
+@cache_page(60 * 15)  # Cache for 15 minutes
 def property_list(request):
     properties = Property.objects.all().values(
         "id", "title", "description", "price_per_night",
         "bedrooms", "bathrooms", "guests",
         "country", "country_code", "category", "favorited", "created_at"
     )
-    return JsonResponse(list(properties), safe=False)
+    return JsonResponse({"data": list(properties)})  # <-- wrap in "data"
+
 
